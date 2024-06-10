e3d629574af5785c52f95503fc8b01939ded1e30
Joshua Sirusstara <joshua.sirusstara@binus.edu>
2024-06-10 12:58:29 +0700

    Add project files.

---
+++ CameraWebSocket/Program.cs
@@ -0,0 +1,57 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using CameraWebSocket;
+using EOSDigital.API;
+using EOSDigital.SDK;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("https://localhost:42069");
+var app = builder.Build();
+
+
+app.UseWebSockets();
+
+
+
+
+
+
+
+app.Map("/ws",async context =>
+{
+
+
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+
+        
+        while (true)
+        {
+            //this.TakePhotoButton_Click();
+            
+            var message = CameraHelper.SnapPhoto();
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            if (ws.State == WebSocketState.Open)
+                await ws.SendAsync(arraySegment,
+                                    WebSocketMessageType.Text,
+                                    true,
+                                    CancellationToken.None);
+            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+            {
+                break;
+            }
+            Thread.Sleep(5000);
+
+        }
+    }
+    else
+    {
+        context.Response.StatusCode= (int)HttpStatusCode.BadRequest;
+    }
+
+});
+
+await app.RunAsync();
